2bb7cbd63ba2fd539a81d7beca12ae1c02a3d388
Stratos Gerakakis <stratosgear@gmail.com>
2018-04-10 19:55:03 +0200

    Commited a failed nuxt SPA app

--- app/client/__init__.py
+++ app/client/__init__.py
@@ -4,11 +4,11 @@ from flask import Blueprint, render_template
 
 client_bp = Blueprint('client_app', __name__,
                         url_prefix='',
-                        static_url_path='/dist',
-                        static_folder='./app/dist',
-                        template_folder='./app/',
+                        static_url_path='/static',
+                        static_folder='../static',
+                        template_folder='../static/spa/dist',
                         )
 
 @client_bp.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.spa.html')
